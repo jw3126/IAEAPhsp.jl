4abf6e487898d6f7e1cf0e1ef451c1c4077b7683
Jan Weidner <jw3126@gmail.com>
2016-12-23 17:17:04 +0100

    fix

--- src/wrapper.jl
+++ src/wrapper.jl
@@ -2,24 +2,22 @@ export Source, Particle, get_particle
 
 
 immutable Source
-    filename::String
+    header_path::String
     id::SourceId
     access::IAEA_I32
     max_particles::Int64
     left_particles::Ref{Int64}
-    function Source(filename, access=1)
+    function Source(header_path, access=1)
         id = ref(IAEA_I32, 1)
-        new_source(filename, id, access)
+        new_source(header_path, id, access)
         max_particles = get_max_particles(id, -1) # -1 counts all particles
         left_particles = Ref(max_particles)
-        new(filename, id, access, max_particles, left_particles)
+        new(header_path, id, access, max_particles, left_particles)
     end
 end
 
-get_used_original_particles(s::Source) = get_used_original_particles(s.id)
 Base.length(s::Source) = value(s.left_particles)
 destroy(s::Source) = destroy_source(s.id)
-
 
 immutable Particle
     typ::IAEA_I32
@@ -37,14 +35,12 @@ end
 
 function get_particle(s::Source)
     typ, E, wt, x, y, z, u, v, w, extra_floats, extra_ints = get_particle(s.id)
+    s.left_particles.x -= 1
     Particle(typ, E, wt, x, y, z, u, v, w, extra_floats, extra_ints)
 end
 
 import Base: start, next, done
 start(s::Source) = nothing
-function next(s::Source, state::Void)
-    p = get_particle(s)
-    s.left_particles.x -= 1
-    p, nothing
-end
+next(s::Source, state::Void) = get_particle(s), nothing
+
 done(s::Source, state::Void) = length(s) <= 0
